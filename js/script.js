9747b642e6b79cd86cb59837c4eace11b44c362b
Kirill <kirillmlynchik@gmail.com>
2021-12-21 20:56:14 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+' use strict ';
+
+const answers = [];
+answers [0] = prompt('What is your name?','');
+answers [1] = prompt('what is oyur last name ?','');
+answers [2] = prompt('How old are you?','');
+
+console.log (answers);
